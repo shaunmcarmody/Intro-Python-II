9ff514aaf03091991d43335dd01123c9be9b6a8a
shaunmcarmody <shaun.carmody@live.co.uk>
2019-05-30 00:02:31 +0100

    respond to commands

--- src/adv.py
+++ src/adv.py
@@ -3,54 +3,90 @@ from player import Player
 
 # Declare all the rooms
 
-room = {
-    'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mount beckons"),
-
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
-
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
-
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
-
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
-}
-
 # Link rooms together
 
-room['outside'].n_to = room['foyer']
-room['foyer'].s_to = room['outside']
-room['foyer'].n_to = room['overlook']
-room['foyer'].e_to = room['narrow']
-room['overlook'].s_to = room['foyer']
-room['narrow'].w_to = room['foyer']
-room['narrow'].n_to = room['treasure']
-room['treasure'].s_to = room['narrow']
+room = {
+    'outside':  Room("Outside Cave Entrance",
+                     "North of you, the cave mount beckons",
+                     "foyer",
+                     None,
+                     None,
+                     None,
+                    ),
 
-#
+    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
+                    passages run north and east.""",
+                    "overlook",
+                    "outside",
+                    "narrow",
+                    None),
+
+    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
+                    into the darkness. Ahead to the north, a light flickers in
+                    the distance, but there is no way across the chasm.""",
+                    None,
+                    "foyer",
+                    None,
+                    None),
+
+    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
+                    to north. The smell of gold permeates the air.""",
+                    "treasure",
+                    None,
+                    None,
+                    "foyer"),
+
+    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
+                    chamber! Sadly, it has already been completely emptied by
+                    earlier adventurers. The only exit is to the south.""",
+                    None,
+                    "narrow",
+                    None,
+                    None),
+}
+
 # Main
-#
 
 # Make a new player object that is currently in the 'outside' room.
 
 player = Player('outside')
+room = room[player.room]
 
-# print(player.room)
-
-print(room[player.room].description)
+# helper function that gives you a list of directions available to you
 
 # Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
+while True:
+# Prints the current room name
+    print(player.room)
+# Prints the current description (the textwrap module might be useful here).
+    # print(room.description)
+# Waits for user input and decides what to do.
+    cmd = input("Please input n/s/e/w: ")
+    if cmd == "q":
+        break
+    elif cmd == "n":
+        player.direction("n")
+        if room.n_to:
+            player.room = room.n_to
+        pass
+    elif cmd == "s":
+        player.direction("s")
+        if room.s_to:
+            player.room = room.s_to
+        pass
+    elif cmd == "e":
+        player.direction("e")
+        if room.e_to:
+            player.room = room.e_to
+        pass
+    elif cmd == "w":
+        player.direction("w")
+        if room.w_to:
+            player.room = room.w_to
+        pass
+    else:
+        print("Input n/s/e/w, or q to quit")
+
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
 #
--- src/player.py
+++ src/player.py
@@ -1,6 +1,11 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
 
+# The player class should be around/no more than 5 lines of code
+
 class Player:
     def __init__(self, room):
         self.room = room
+
+    def direction(self, d):
+        print(d)
